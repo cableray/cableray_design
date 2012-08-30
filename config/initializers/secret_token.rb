db7828c039fcff8dfc77162e3e7785cbfbbd9d99
cableray <cableraywire@gmail.com>
2012-08-29 18:09:48 -0600

    refinery cms rails app install.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CablerayDesign::Application.config.secret_token = 'b078d12ba54a980d4532809c100a1156e72dc8c3938fde260ed5cc7d990d56856e3195dbda825a166bcd7bf2a7cca185989db3d0663e29ed450b92b8190911f0'
