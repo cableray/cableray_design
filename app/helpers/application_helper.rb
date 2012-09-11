84816080532f8b550ebd0cbd7965976205c92b3c
cableray <cableraywire@gmail.com>
2012-09-11 01:35:50 -0600

    Spit title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def split_title(title, as_html=true)
+    title, subtitle = title.split(' - ', 2)
+    if as_html then "#{title} <small>#{subtitle}</small>" else [title, subtitle] end
+  end
+  
+  def only_title(title)
+    split_title(title,false)[0]
+  end
+  
+  def only_subtitle(title)
+    split_title(title,false)[1]
+  end
+  
 end
